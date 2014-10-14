72f89c827de55a3924328f3ecbedf2cf2b2f575a
oppakub <kasidis99@gmail.com>
2014-10-14 23:06:13 +0700

    add Laravel framework

---
+++ webapp/laravel/app/routes.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+//Basic GET Route
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+//Basic GET Route
+Route::get('foo/bar', function()
+{
+	return 'Hello GET foo bar!';
+});
+
+//Basic POST Route
+Route::post('foo/bar', function()
+{
+	return 'Hello POST foo bar!';
+});
+
+//Registering A Route For Multiple Verbs
+Route::match(array('GET', 'POST'), 'get/post', function()
+{
+	return 'Hello GET & POST';
+});
+
+//Registering A Route Responding To Any HTTP Verb
+Route::any('foo', function()
+{
+    return 'Hello any HTTP foo World';
+});
+
+//Forcing A Route To Be Served Over HTTPS
+Route::get('foos', array('https', function()
+{
+    return 'foo Must be over HTTPS';
+}));
+
+Route::get('user/{id}', function($id)
+{
+	return 'User page '.$id;
+});
+
+
