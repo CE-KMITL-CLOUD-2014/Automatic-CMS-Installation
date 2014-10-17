b31c65526db8bc3a9df3b2fde4cebc5aff043f9e
oppakub <kasidis99@gmail.com>
2014-10-18 01:24:47 +0700

    test connect to azure db

--- webapp/laravel/app/views/db.php
+++ webapp/laravel/app/views/db.php
@@ -1,14 +1,3 @@
 <?php
-$server = "tcp:dh96f1a9t7.database.windows.net,1433";
-$user = "CMS-South-Asia-Server@dh96f1a9t7";
-$pwd = "LazPe%Zg3MqjHVs*z4=bJX(W#-Br3eGRMg/m";
-$db = "nfcmsservice";
-
-try{
-    $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(print_r($e));
-}
+$pdo = new PDO("dblib:host=dh96f1a9t7.database.windows.net;dbname=nfcmsservice", 'CMS-South-Asia-Server@dh96f1a9t7', 'LazPe%Zg3MqjHVs*z4=bJX(W#-Br3eGRMg/m');
 ?>
